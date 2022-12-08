33d23a49d52de98cbff968f8f4ec5c35d234c755
航 遠藤 <w.endo@jc-21.jp>
2022-12-08 10:10:32 +0900

    向いてる方向に移動

--- Tank.cpp
+++ Tank.cpp
@@ -1,6 +1,7 @@
 #include "Tank.h"
 #include "Engine/Model.h"
 #include "Engine/Input.h"
+#include "Engine/Debug.h"
 
 //コンストラクタ
 Tank::Tank(GameObject* parent)
@@ -32,6 +33,33 @@ void Tank::Update()
     {
         transform_.rotate_.y += 2;
     }
+
+    Debug::Log("angle =");
+    Debug::Log(transform_.rotate_.y, true);
+
+    //戦車の現在地をベクトル型に変換
+    XMVECTOR vPos = XMLoadFloat3(&transform_.position_);
+
+    //1フレームの移動ベクトル
+    XMVECTOR vMove{ 0.0f, 0.0f, 0.1f, 0.0f };   //奥に0.1m
+
+    //transform_.rotate_.y度回転させる行列を作成
+    XMMATRIX mRotY = XMMatrixRotationY(XMConvertToRadians(transform_.rotate_.y));
+
+    //移動ベクトルを変形（戦車と同じ向きに回転）させる
+    vMove = XMVector3TransformCoord(vMove, mRotY);
+
+    //Wキーが押されたら
+    if (Input::IsKey(DIK_W))
+    {
+        //移動
+        vPos += vMove;
+
+        //現在地をベクトルからいつものtranform_.position_に戻す
+        XMStoreFloat3(&transform_.position_, vPos);
+    }
+
+
 }
 
 //描画
